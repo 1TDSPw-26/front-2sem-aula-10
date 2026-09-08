43b0253eb196ccba365a423338802c38e112b271
adaltonagabe <adalto.nagabe@gmail.com>
2026-09-08 20:58:43 -0300

    feat: Estrutura de rotas criada no main

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router';
 import App from './App.tsx'
 
+import { createBrowserRouter, RouterProvider } from 'react-router';
 import Produtos from './routes/Produtos/index.tsx';
 import Home from './routes/Home/index.tsx';
 import EditarProdutos from './routes/EditarProdutos/index.tsx';
@@ -25,5 +25,5 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>
+  </StrictMode>,
 )
