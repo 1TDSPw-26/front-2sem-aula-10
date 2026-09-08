7502455dd86b8875f93343b6416ae035c74496a4
Ludmylla1905 <ludmilanogueira231@gmail.com>
2026-09-08 20:58:58 -0300

    feat: estrutura de rotas criadas na main

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -2,13 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
+import { createBrowserRouter, RouterProvider } from 'react-router'
 import Home from './routes/Home/index.tsx'
-import Produto from './routes/Produtos/index.tsx'
+import Produtos from './routes/Produtos/index.tsx'
 import EditarProduto from './routes/EditarProdutos/index.tsx'
 import Error from './routes/Error/index.tsx'
 
+const router = createBrowserRouter([
+  {path: "/", element: <App/>, errorElement:<Error/>, children:[
+  {path: "/", element: <Home/>},
+  {path: "/produtos", element: <Produtos/>},
+  {path: "/editar-produtos", element: <EditarProduto/>}
+  ] }
+]);
+
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
