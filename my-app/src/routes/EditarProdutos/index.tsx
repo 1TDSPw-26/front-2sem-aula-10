40100ff46ac5ba51ab2f1bf809227df0b4abc644
pedroamarop <pedro.a.p.dev@gmail.com>
2026-09-08 20:49:33 -0300

    feat: criação de componentes com rotas

--- my-app/src/routes/EditarProdutos/index.tsx
+++ my-app/src/routes/EditarProdutos/index.tsx
@@ -1,8 +1,5 @@
-export default function App() {
-  return (
-    <div>
-      <h1>Editar Produtos</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita hic quos recusandae mollitia rerum amet optio tempore perferendis impedit pariatur at doloribus dolore quidem harum ab vero quod, dignissimos necessitatibus.</p>
-    </div>
-  )
+export default function EditarProdutos() {
+    return (
+        <div></div>
+    )
 }
--- my-app/src/routes/home/index.tsx
+++ my-app/src/routes/home/index.tsx
@@ -1,8 +1,5 @@
 export default function Home() {
     return (
-        <div>
-            <h1>Home</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex architecto perferendis a aut molestias velit ipsa dolor quidem itaque, officiis, nobis ut, quam repellat. Aperiam quae atque voluptas culpa at.</p>
-        </div>
+        <div></div>
     )
 }
