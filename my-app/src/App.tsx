303894892add58e282f57386848b85e1fed07b09
NickRM22 <nick.rodrigues2005@gmail.com>
2026-09-08 20:49:32 -0300

    feat: Criação de componentes de rotas

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,6 +2,7 @@ import Cabecalho from "./components/Cabecalho";
 import Conteudo from "./components/Conteudo";
 import Rodape from "./components/Rodape";
 
+
 export default function App() {
   return (
     <div>
---
+++ my-app/src/routes/EditarProdutos/index.tsx
@@ -0,0 +1,6 @@
+export default function EditarProdutos() {
+    return(<main>
+        <h1>Editar Produtos</h1>
+    </main>
+    )
+}
