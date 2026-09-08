24643c03eaf60be90c76dd96e1a1dc8913a4d9db
eneiqmaneiro <henriquetacou@gmail.com>
2026-09-08 20:58:46 -0300

    feat : Estrutura de rotas criadas na main.

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+
 import { createBrowserRouter, RouterProvider} from 'react-router'
+
 import Home from './routes/Home/index.tsx'
 import Produtos from './routes/Produtos/index.tsx'
 import EditarProdutos from './routes/EditarProdutos/index.tsx'
