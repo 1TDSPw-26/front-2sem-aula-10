02a964b78b81fea08ca057f54a712cb451097fff
pedroamarop <pedro.a.p.dev@gmail.com>
2026-09-08 21:06:19 -0300

    refactor: atualiza estrutura de route

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -5,25 +5,7 @@ import App from './App.tsx'
 import Home from './routes/Home';
 import Produtos from './routes/Produtos';
 import EditarProdutos from './routes/EditarProdutos';
-import Error from './routes/Error';
-
-const router = createBrowserRouter([
-  {
-    path: "/",  element: <App />,  errorElement: <Error />, children: [
-      { path: "/", element: <Home /> },
-      { path: "/produtos", element: <Produtos /> },
-      { path: "/editar-produtos", element: <EditarProdutos /> }
-    ]
-  }
-]);
-
-
-import Home from './routes/home/index';
-import Produtos from './routes/Produtos/index';
-import EditarProdutos from './routes/EditarProdutos/index';
-import Error from './routes/Error/index';
-
-import { createBrowserRouter, RouterProvider } from 'react-router';
+import Error from './routes/Errors';
 
 const router = createBrowserRouter([
   {
