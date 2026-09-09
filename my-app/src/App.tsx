a7a36439ab961da6321491d027c892e9c51ad46c
Ludmylla1905 <ludmilanogueira231@gmail.com>
2026-09-08 21:04:37 -0300

    feat: adicionando outlet na app

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,5 @@
+import { Outlet } from "react-router";
 import Cabecalho from "./components/Cabecalho/Cabecalho";
-import Conteudo from "./components/Conteudo/Conteudo";
 import Rodape from "./components/Rodape/Rodape";
 
 
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div>
       <Cabecalho/>
-      <Conteudo/>
+      <Outlet/>
       <Rodape/>
     </div>
   )
