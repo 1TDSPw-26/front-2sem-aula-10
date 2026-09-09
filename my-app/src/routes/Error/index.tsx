04859fd7463664be9679740494d1a583d8eb357c
adaltonagabe <adalto.nagabe@gmail.com>
2026-09-08 21:15:04 -0300

    feat : Editado o arquivo index.txs da rota Error

--- my-app/src/routes/Error/index.tsx
+++ my-app/src/routes/Error/index.tsx
@@ -1,7 +1,7 @@
 export default function Error(){
     return (
         <main>
-            <h2>Pagina nao encontrada</h2>
+            <h2>404 - Pagina nao encontrada</h2>
         </main>
     )
 }
