27907593430906331514535031f8f48bfcc8d05b
lucas-silva8077 <lucasfernando8077@gmail.com>
2026-09-08 21:09:21 -0300

    feat: Criação de rotas finalizada

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,21 +1,12 @@
+import { Outlet } from "react-router";
 import Cabecalho from "./components/Cabecalho";
-import Conteudo from "./components/Conteudo";
 import Rodape from "./components/Rodape";
-import EditarProdutos from "./routes/EditarProdutos";
-import Home from "./routes/Home";
-import Produtos from "./routes/Produtos";
-
-
 
 export default function App() {
     return (
         <div>
-            <App/>
-            <EditarProdutos/>
-            <Home/>
-            <Produtos/>
             <Cabecalho />
-            <Conteudo />
+            <Outlet />
             <Rodape />
         </div>
     );
--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+
 import Home from './routes/Home/index.tsx'
 import Error from './routes/Error/index.tsx'
 import Produtos from './routes/Produtos/index.tsx'
 import EditarProdutos from './routes/EditarProdutos/index.tsx'
-import { createBrowserRouter, Router, RouterProvider } from 'react-router'
+import { createBrowserRouter, RouterProvider } from 'react-router'
 
 
 const Router = createBrowserRouter([{
